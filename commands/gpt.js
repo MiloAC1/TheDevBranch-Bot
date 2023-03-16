2c19abacce643e5467fd23dbbc58e3d7c26356c3
ponka <johntindall9@gmail.com>
2023-03-15 23:41:33 -0500

    Update gpt.js

--- commands/gpt.js
+++ commands/gpt.js
@@ -15,7 +15,7 @@ module.exports = {
         max_tokens: 4000,
         n: 1,
         stop: '\n',
-        temperature: 0.7
+        temperature: 1
       }, {
         headers: {
           'Content-Type': 'application/json',
