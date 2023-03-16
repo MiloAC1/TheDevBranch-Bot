c638c550cf0f2a31a2f10eda1e3e8745f90cf039
ponka <53353185+ponka07@users.noreply.github.com>
2023-03-15 20:46:53 -0500

    added gpt command

---
+++ commands/gpt.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { openai } = require('../config.json'); // load API key from config file
+const axios = require('axios');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('gpt')
+    .setDescription('Ask ChatGPT anything')
+    .addStringOption(option => option.setName('input').setDescription('The input to process')),
+  async execute(interaction) {
+    const input = interaction.options.getString('input');
+    try {
+      const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        prompt: input,
+        max_tokens: 150,
+        n: 1,
+        stop: '\n',
+        temperature: 0.7
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${openai.apiKey}`
+        }
+      });
+      const message = response.data.choices[0].text;
+      await interaction.reply(message);
+    } catch (error) {
+      console.log(error);
+      await interaction.reply('Something went wrong');
+    }
+  },
+};
