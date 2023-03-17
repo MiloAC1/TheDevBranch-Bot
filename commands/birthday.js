6492369eedbfad0ac3e8d3654d081d086c018f52
MiloAC <crabapplefarms@icloud.com>
2023-03-17 18:20:24 -0500

    Update birthday.js

--- commands/birthday.js
+++ commands/birthday.js
@@ -17,15 +17,15 @@ module.exports = {
             subcommand
                 .setName('set-birthday')
                 .setDescription('Set your')
-                .addStringOption(option =>
+                .addIntegerOption(option =>
                     option.setName('month')
                         .setDescription('Enter your month')
                         .setRequired(true))
-                .addStringOption(option =>
+                .addIntegerOption(option =>
                     option.setName('day')
                         .setDescription('Enter your birthday in the format MM/DD')
                         .setRequired(true))
-                .addStringOption(option =>
+                .addIntegerOption(option =>
                     option.setName('year')
                         .setDescription('Enter your birthday in the format'))),
     async execute(interaction) {
@@ -38,9 +38,9 @@ module.exports = {
                 await interaction.reply(`Sorry, I couldn't find ${user.username}'s birthday.`);
             }
         } else if (interaction.options.getSubcommand() === 'set-birthday') {
-            const Day = interaction.options.getString('day');
-            const Month = interaction.options.getString('month');
-            const Year = interaction.options.getString('year')
+            const Day = interaction.options.getInteger('day');
+            const Month = interaction.options.getInteger('month');
+            const Year = interaction.options.getInteger('year')
 
             if (Year == null) {
                  date = Month + '-' + Day 
@@ -69,11 +69,11 @@ module.exports = {
             try {
                 fs.writeFileSync('./birthdays.json', JSON.stringify(birthdays));
                 await interaction.reply(`Your birthday has been recorded as ${date}.`);
-                const dateString = date;
-                const [monthString, dayString, yearString] = dateString.split("/");
-                const month = parseInt(monthString);
-                const day = parseInt(dayString);
-                const year = parseInt(yearString);
+                //const dateString = date;
+                //const [monthString, dayString, yearString] = dateString.split("/");
+                //const month = parseInt(monthString);
+                //const day = parseInt(dayString);
+                //const year = parseInt(yearString);
             } catch (err) {
                 console.error(err);
                 await interaction.reply(`There was an error recording your birthday.`);
