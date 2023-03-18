6acae264e3c8c773599e233c728044b53e9ecb24
ponka <johntindall9@gmail.com>
2023-03-17 23:03:17 -0500

    Create yt.js

---
+++ commands/yt.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+const ytdl = require('ytdl-core');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('youtube')
+        .setDescription('Plays a youtube video in a voice channel')
+        .setUsage('<youtube url>')
+        .setAction(async (message, args) => {
+        const voiceChannel = message.member.voice.channel;
+        if (!voiceChannel) return message.reply('You must be in a voice channel to use this command');
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) return message.reply('I need the permissions to join and speak in your voice channel');
+        const connection = await voiceChannel.join();
+        const dispatcher = connection.play(ytdl(args[0], { filter: 'audioonly' }));
+        dispatcher.on('finish', () => voiceChannel.leave());
+        })
+}
